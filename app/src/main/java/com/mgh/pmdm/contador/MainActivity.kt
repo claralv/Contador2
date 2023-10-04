8eaf026159b5e790f1c99ffc50426fc9ea562de9
claralv <lagunas.informatica@gmail.com>
2023-10-04 09:41:20 +0200

    Añadido un comentario

--- app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
+++ app/src/main/java/com/mgh/pmdm/contador/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.TextView
 class MainActivity : AppCompatActivity() {
 
     var contador=0
+    //Esto es un comentario
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
